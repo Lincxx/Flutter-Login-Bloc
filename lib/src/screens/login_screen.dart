b1a080c108259c0d059c9e571500cbcc91808efa
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-01-16 16:41:34 -0500

    Adding in the basic bloc logic

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 20.0),),
+          submitButton()
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@email.com',
+        labelText: 'Email Address'
+      ),
+    );
+  }
+
+   Widget passwordField(){
+    return TextField(
+      //obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'password',
+        labelText: 'Password'
+      ),
+    );
+  }
+
+  Widget submitButton(){
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
