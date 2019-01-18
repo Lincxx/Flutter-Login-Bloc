9636bc4d7e36a6098fcc9a05519c5ea448173b26
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-01-18 11:41:49 -0500

    Now listening to multiple streams for our validation

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,17 +1,27 @@
 import 'dart:async';
 import 'validators.dart';
 
+import 'package:rxdart/rxdart.dart';
+
 class Bloc with Validators {
-  final _email = StreamController<String>();
-  final _password = StreamController<String>();
+  final _email = StreamController<String>.broadcast();
+  final _password = StreamController<String>.broadcast();
 
   //Add data to a stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
 
-  //Change data
+  //Returns a combine stream
+  Stream<bool> get submitValid => Observable.combineLatest2(email, password, (e, p) => true);
+
+  //Change data - sink puts the value back into the stream
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+
+  submit() {
+    
+  }
 
   //Clean up 
   dispose(){
@@ -21,4 +31,4 @@ class Bloc with Validators {
 }
 
 
-final bloc = Bloc();
+
